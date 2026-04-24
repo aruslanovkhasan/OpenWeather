30e4823e90b94ed5a164d3be10a1c1d81c123fc0
aruslanovkhasan <aruslanovkhasan@gmail.com>
2026-04-24 18:55:29 +0500

    fixed api problems

--- src/store/index.js
+++ src/store/index.js
@@ -15,7 +15,7 @@ const store = createStore({
 
         async getWeather({ commit, state }, city) {
             try {
-                const res = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${state.apiKey}`)
+                const res = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${state.apiKey}`)
                 const weather = res.data[0]
                 const { lat, lon } = weather
                 const res2 = await axios.get(`https://api.openweathermap.org/data/2.8/onecall?lat=${lat}&lon=${lon}&exclude=hourly,alerts,minutely&appid=${state.apiKey}&units=metric&lang=ru`)
