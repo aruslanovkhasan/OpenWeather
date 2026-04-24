f7af8f0b2246e42e8e8e9ae0b8cfb48f8c460dcd
aruslanovkhasan <aruslanovkhasan@gmail.com>
2026-04-24 21:41:40 +0500

    fix api proxy

--- api/geo.js
+++ api/geo.js
@@ -1,8 +1,8 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     const { city } = req.query
 
     const response = await fetch(
-        `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${state.apiKey}`
+        `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${process.env.OPENWEATHER_API_KEY}`
     )
     const data = await response.json()
     res.json(data)
--- api/weather.js
+++ api/weather.js
@@ -1,8 +1,8 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     const { lat, lon } = req.query
 
     const response = await fetch(
-        `https://api.openweathermap.org/data/2.8/onecall?lat=${lat}&lon=${lon}&exclude=hourly,alerts,minutely&appid=${state.apiKey}&units=metric&lang=ru`
+        `https://api.openweathermap.org/data/2.8/onecall?lat=${lat}&lon=${lon}&exclude=hourly,alerts,minutely&appid=${process.env.OPENWEATHER_API_KEY}&units=metric&lang=ru`
     )
     const data = await response.json()
     res.json(data)
